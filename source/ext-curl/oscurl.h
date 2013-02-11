e2c8350c443aca4550e492adcabf968965f98bb8
Evgeniy Golovin <e.golovin@gemotest.ru>
2013-02-12 02:53:46 +0400

    working on CURL library

---
+++ source/ext-curl/oscurl.h
@@ -0,0 +1,13 @@
+#ifndef OSCURL_H
+#define OSCURL_H
+
+#include "../../source/objectscript.h"
+#include "../../source/os-binder.h"
+
+namespace ObjectScript {
+
+void initCurlLibrary(OS* os);
+
+};
+
+#endif
