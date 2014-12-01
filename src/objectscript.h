b69d9ff5da2258f87533559a6f552889d87c361c
Evgeniy Golovin <evgeniy.golovin@unitpoint.ru>
2014-12-02 01:30:59 +0300

    OS 2.6.3.1-rc, inc version due to OS-FCGI upgraded

--- src/objectscript.h
+++ src/objectscript.h
@@ -74,7 +74,7 @@ inline void operator delete(void *, void *){}
 #define OS_DEBUG_VERSION
 #endif
 
-#define OS_VERSION		OS_TEXT("2.6.3-rc") OS_PLATFORM_BITS_VERSION OS_DEBUG_VERSION
+#define OS_VERSION		OS_TEXT("2.6.3.1-rc") OS_PLATFORM_BITS_VERSION OS_DEBUG_VERSION
 #define OS_COPYRIGHT	OS_TEXT("OS ") OS_VERSION OS_TEXT(" Copyright (C) 2012-2014 by Evgeniy Golovin")
 #define OS_OPENSOURCE	OS_TEXT("ObjectScript is free and open source: https://github.com/unitpoint/objectscript")
 
